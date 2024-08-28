276c5281b5ef6e9b0b8f54cf084de612463925b6
ericbidopa <ericbidopa@gmail.com>
2024-08-28 03:02:42 +0000

    starting work on inbound screen podcast Item screen

---
+++ components/PodcastItemScreen/PodcastItemPage.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PodcastItemPage = () => {
+  return (
+    <View>
+      <Text>PodcastItemPage</Text>
+    </View>
+  )
+}
+
+export default PodcastItemPage
+
+const styles = StyleSheet.create({})
