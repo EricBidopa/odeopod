11aa95e1f27d7a930a576681735fe4ed5e7cf78e
ericbidopa <ericbidopa@gmail.com>
2024-08-31 03:01:44 +0000

    it is a brand new day and I am starting work on SellModal for people to sell their tokens

---
+++ OrdersScreen/SellModal.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SellModal = () => {
+  return (
+    <View>
+      <Text>SellModal</Text>
+    </View>
+  )
+}
+
+export default SellModal
+
+const styles = StyleSheet.create({})
