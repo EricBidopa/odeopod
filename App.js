f4bb9907d95f991bd94a5213b0753b34f1e11150
ericbidopa <ericbidopa@gmail.com>
2024-09-05 03:30:57 +0000

    working on privy authentication

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
-import { StatusBar } from "expo-status-bar"
-import AppNavigator from "./navigation/AppNavigator"
+// Import required polyfills first
+import "fast-text-encoding";
+import "react-native-get-random-values";
+import "@ethersproject/shims";
+import { StatusBar } from "expo-status-bar";
+import AppNavigator from "./navigation/AppNavigator";
+import React from "react";
+import { PrivyProvider } from "@privy-io/expo";
 
 const App = () => {
   return (
-    <AppNavigator />
-  )
-}
+    <PrivyProvider
+      appId={"cm0kil88x0471x4zrp3fus7ai"}
+      clientId={"client-WY5axK2yKpNuT3nvYrHgYBGhSBUrRfHGtsntrPULY3BK8"}
+    >
+      <AppNavigator />
+    </PrivyProvider>
+  );
+};
 
 export default App;
