b3b2390fe675d78ff90ecce8f5116a948c7bfa23
ericbidopa <ericbidopa@gmail.com>
2024-09-05 06:16:31 +0000

    well, I am making some progress with the privy auth. wish there could be easier ways

--- OnboardingScreen/OnboardingPage.js
+++ OnboardingScreen/OnboardingPage.js
@@ -1,29 +1,44 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { usePrivy } from "@privy-io/expo";
+import { useLoginWithOAuth, usePrivy } from "@privy-io/expo";
+import { useNavigation } from "@react-navigation/native";
 
-const OnboardingPage = ({ navigation }) => {
+const OnboardingPage = () => {
+  const { user } = usePrivy();
+  const navigation = useNavigation();
 
-  
+  const { login, state } = useLoginWithOAuth({
+    onSuccess(user, isNewUser) {
+      if (isNewUser) {
+        navigation.navigate("ChooseUsernamePage");
+      }
+      if (!isNewUser) {
+        navigation.navigate("HomePage");
+      }
+    },
+
+    // },
+    onError(error) {
+      console.log("Error signing in", error);
+    },
+  });
+
   const handleCreateAccount = async () => {
     try {
-      await login();
-      navigation.navigate("ChooseUsernamePage");
+      await login({ provider: "google" });
     } catch (error) {
       console.error("Login failed:", error);
     }
   };
-  
+
   const handleLoginIn = async () => {
     try {
-      await login();
-      navigation.navigate("HomePage");
+      await login({ provider: "google" });
     } catch (error) {
       console.error("Login failed:", error);
     }
   };
-  
 
   return (
     <View style={styles.container}>
@@ -37,12 +52,29 @@ const OnboardingPage = ({ navigation }) => {
           </Text>
         </View>
         <View>
-          <Pressable style={styles.buttons} onPress={handleCreateAccount}>
-            <Text>Create Account</Text>
+          <Pressable
+            style={styles.buttons}
+            onPress={handleCreateAccount}
+            disabled={state.status === "loading"}
+          >
+            <Text>
+              {state.status === "loading" ? "Loading.." : "Create Account"}
+            </Text>
           </Pressable>
-          <Pressable style={styles.buttons} onPress={handleLoginIn}>
-            <Text>Login</Text>
+          <Pressable
+            style={styles.buttons}
+            onPress={handleLoginIn}
+            disabled={state.status === "loading"}
+          >
+            <Text>{state.status === "loading" ? "Loading" : "Login..."}</Text>
           </Pressable>
+
+          {state.status === "error" && (
+            <>
+              <Text style={{ color: "red" }}>There was an error</Text>
+              <Text style={{ color: "lightred" }}>{state.error.message}</Text>
+            </>
+          )}
         </View>
       </View>
     </View>
